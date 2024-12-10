c83484835c48714c363ee2b8cbf3d7841033c175
tiwwi <aoc@mojo.rip>
2024-12-11 00:21:48 +0100

    Whee

--- src/Day08.hs
+++ src/Day08.hs
@@ -1,19 +1,33 @@
 module Day08 (solve) where
 
-import Data.Text qualified as T
-import Data.Text.IO qualified as T
+import Helpers.Matrix
+import qualified Data.Map as M
+import qualified Data.Set as S
+import Helpers.List
 
-type Input = [T.Text]
+data Roof = Roof {bds::(Pos, Pos), antennas::M.Map Char [Pos]}
 
 solve :: FilePath -> IO (String, String)
 solve fname = do
-    txt <- T.readFile fname
+    txt <- readFile fname
     let input = parseInput txt
     return (show $ part1 input, show $ part2 input)
 
-parseInput = undefined
+parseInput str = Roof (bounds mat) out
+    where mat = readAOCMatrix str
+          out = M.fromListWith (++) [(c, [pos]) | (pos, c) <- assocs mat, c /= '.']
 
-part1 :: Input -> String
-part1 = const "unfinished"
-part2 :: Input -> String
-part2 = const "unfinished"
+findAntinodes :: (Roof -> (Pos, Pos) -> [Pos]) -> Roof -> S.Set Pos
+findAntinodes f roof = foldMap go $ M.elems (antennas roof)
+    where go positions = S.fromList $ allPairs positions >>= (f roof)
+
+antinodes1 (Roof bds _) (a,b) = filter (inRange bds) [(2*^a) - b, (2*^b) - a]
+antinodes2 (Roof bds _) (a,b) = takeWhile (inRange bds) [a + k*^(a-b) | k <- [0..]] ++
+                            takeWhile (inRange bds) [b + k*^(b-a) | k <- [0..]] 
+
+parts :: (Roof -> (Pos, Pos) -> [Pos]) -> Roof -> Int
+parts f = S.size . findAntinodes f
+
+part1, part2 :: Roof -> Int
+part1 = parts antinodes1
+part2 = parts antinodes2
