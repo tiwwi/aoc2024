654c9e8ff366eb825d6216699575cec6671f2426
Maximilian Tito Wittmann <maximilian.wittmann@hpi.de>
2024-12-30 22:59:38 +0100

    Reduced runtime of Day19 a lil

--- src/Day19.hs
+++ src/Day19.hs
@@ -8,6 +8,7 @@ import Data.Char
 import Data.Vector qualified as V
 import Helpers.ByteString
 import Helpers.List (count)
+import Data.Set qualified as S
 
 type Towel = B.ByteString
 type Design = B.ByteString
@@ -15,8 +16,10 @@ type Design = B.ByteString
 solve :: FilePath -> IO (String, String)
 solve fname = do
   txt <- B.readFile fname
-  let (towels, designs) = quickParseB inputP txt
-      nums = findNum towels <$> designs
+  let (_towels, designs) = quickParseB inputP txt
+      maxTowel = maximum $ B.length <$> _towels
+      towels = S.fromList _towels
+      nums = findNum maxTowel towels <$> designs
   return (show $ count (/= 0) nums, show $ sum nums)
 
 inputP :: A.Parser ([Towel], [Design])
@@ -32,13 +35,13 @@ towelP = A.takeWhile1 isAlpha
 designP :: A.Parser Design
 designP = towelP
 
-findNum :: [Towel] -> Design -> Int
-findNum towels design = dpTable V.! B.length design
+findNum :: Int -> S.Set Towel -> Design -> Int
+findNum maxTowel towels design = dpTable V.! B.length design
   where
     dpTable = V.generate (B.length design + 1) indexF
     tailsV = V.fromList $ reverse $ B.tails design
     indexF 0 = 1
     indexF i =
       let subDesign = tailsV V.! i
-          options = sum $ [dpTable V.! (i - B.length towel) | towel <- towels, B.isPrefixOf towel subDesign]
+          options = sum $ [dpTable V.! (i - cutLength) | (cutLength, prefix) <- zip [1..maxTowel] (tail $ B.inits subDesign), S.member prefix towels]
        in options
